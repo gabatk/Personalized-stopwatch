3691f0afb1146b79ea706279fd39819214db2164
gabatk <gabriela.trybuszek@gmail.com>
2023-01-07 17:44:23 +0100

    Reset function completed.

--- js/main.js
+++ js/main.js
@@ -46,6 +46,16 @@ const handleStop = () => {
 		timesArr.push(stopwatch.textContent);
 	}
 
+	clearStuff();
+};
+
+const handleReset = () => {
+	time.style.visibility = 'hidden';
+	timesArr = [];
+	clearStuff();
+};
+
+const clearStuff = () => {
 	clearInterval(countTime);
 	stopwatch.textContent = '0:00';
 	timeList.textContent = '';
@@ -56,3 +66,4 @@ const handleStop = () => {
 startBtn.addEventListener('click', handleStart);
 pauseBtn.addEventListener('click', handlePause);
 stopBtn.addEventListener('click', handleStop);
+resetBtn.addEventListener('click', handleReset);
