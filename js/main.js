9e167ea060acac91cb54c1afe9dbc2f128312b88
gabatk <gabriela.trybuszek@gmail.com>
2023-01-07 12:40:00 +0100

    JavaScript variables prepared.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const stopwatch = document.querySelector('.stopwatch');
+const time = document.querySelector('.time');
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const timeList = document.querySelector('.time-list');
+const infoBtn = document.querySelector('.info');
+const modalShadow = document.querySelector('.modal-shadow');
+const closeModalBtn = document.querySelector('.close');
+
