6316b2b7eaae6ce8f8e6d1d4075eea83284290a9
gabatk <gabriela.trybuszek@gmail.com>
2023-01-07 12:42:22 +0100

    Countdown function completed. Start button.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ const infoBtn = document.querySelector('.info');
 const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
 
+let countTime;
+let minutes = 0;
+let seconds = 0;
+
+const handleStart = () => {
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:0${seconds}`;
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:${seconds}`;
+		} else {
+			minutes++;
+			seconds = 0;
+			seconds++;
+			stopwatch.textContent = `${minutes}:00`;
+		}
+	}, 1000);
+};
+
+startBtn.addEventListener('click', handleStart);
